6ff4304d7558f3c13f240a77bee700ae565ded6f
anton.datskovskiy <datskovskiy.anton@gmail.com>
2021-04-07 15:39:28 +0300

    Implement code

--- src/data_structure-types.js
+++ src/data_structure-types.js
@@ -1,11 +1,29 @@
 const validateTitle = (value) => {
-  // Student implementation
-  return;
+  if (typeof value !== 'string')
+    return 'Incorrect input data';
+
+  return value.length > 2 && value.length < 20 && /[A-Z]/.test(value.charAt(0))
+    ? 'VALID'
+    : 'INVALID';
 }
 
 const sum = (value1, value2) => {
-  // Student implementation
-  return;
+  let argNumber = 0;
+  let arg2 = 0;
+
+  if (typeof value1 === 'number') {
+    argNumber = value1;
+    arg2 = parseInt(value2);
+  }
+  else {
+    argNumber = value2;
+    arg2 = parseInt(value1);
+  }
+
+  if (argNumber % 5 === 0)
+    argNumber *= -1;
+
+  return argNumber + arg2;
 };
 
 module.exports = {
